5dacfbf6eb42ad4b640133dd7d1911ac816af6a2
Jonathan Leventhal <jonathanleventhal@Jonathans-MacBook-Pro.local>
2020-03-31 16:32:09 -0400

    More fixes

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const http = require('http');
-var dt = require('./myfirstmodule');
+var dt = require('./node_modules/myfirstmodule');
 
-const hostname = '127.0.0.1';
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8080;
-}
-app.listen(port);
+
 
 const server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());
   res.end();
 })
+
+const hostname = '127.0.0.1';
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
+server.listen(port);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- myfirst.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end('Hello World!');
-}).listen(8080);
